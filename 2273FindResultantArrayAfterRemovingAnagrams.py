5cdb7827163a1ff09e6d280a2eec4deaa742f8c0
Shwetha K <shweetha.sweety17@gmail.com>
2025-10-14 12:44:28 +0530

    25-10-13 :: October 2025 13 : Shwetha K : 2273 Find Resultant Array After Removing Anagrams

--- 2273FindResultantArrayAfterRemovingAnagrams.py
+++ 2273FindResultantArrayAfterRemovingAnagrams.py
@@ -4,12 +4,15 @@ You are given a 0-indexed string array words, where words[i]
  consists of lowercase English letters.
 
 In one operation, select any index i 
-such that 0 < i < words.length and words[i - 1] and words[i] are anagrams, 
-and delete words[i] from words. Keep performing this operation as long as 
+such that 0 < i < words.length and words[i - 1] and words[i] 
+are anagrams, 
+and delete words[i] from words. Keep performing this operation 
+as long as 
 you can select an index that satisfies the conditions.
 
 Return words after performing all operations. 
-It can be shown that selecting the indices for each operation in any arbitrary
+It can be shown that selecting the indices for each operation 
+in any arbitrary
 order will lead to the same result.
 
 An Anagram is a word or phrase formed 
