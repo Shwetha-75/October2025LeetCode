7d24e85674f9c3888f71ef2f9cf7c24bcae3f073
Shwetha K <shweetha.sweety17@gmail.com>
2025-10-27 17:19:18 +0530

    25-10-27 :: October 2025 27 : Shwetha K : Number of Laser Beams in a Bank

---
+++ one.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} balance
+ */
+var Bank = function(balance) {
+    this.n=balance.length;
+    this.balance=balance
+};
+
+/** 
+ * @param {number} account1 
+ * @param {number} account2 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.transfer = function(account1, account2, money) {
+    if(account1>this.n || account2>this.n || this.balance[account1-1]<money) return false 
+    this.balance[account1-1]-=money 
+    this.balance[account2-1]+=money 
+    return true     
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.deposit = function(account, money) {
+    if(account>this.n) return false 
+    this.balance[account-1]+=money 
+    return true
+};
+
+/** 
+ * @param {number} account 
+ * @param {number} money
+ * @return {boolean}
+ */
+Bank.prototype.withdraw = function(account, money) {
+    if(account>this.n || this.balance[account-1]<money) return false 
+    this.balance[account-1]-=money 
+    return true  
+};
+
